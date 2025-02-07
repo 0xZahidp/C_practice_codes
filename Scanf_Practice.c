942ffe9827534e13d02af69c3b21b715b91fa1dd
Md Zahid Hasan Patwary <82576513+0xZahidp@users.noreply.github.com>
2025-02-07 21:50:41 +0600

    Add files via upload

---
+++ Scanf_Practice.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int num,num2;
+    float num3;
+    char ch;
+    printf("Enter a character: \n");
+    scanf("%c",&ch);
+    printf("Your entered character is: %c \n", ch);
+    printf("Enter 2 integer num: \n");
+    scanf("%d %d", &num, &num2);
+    printf("You have entered: %d %d \n",num, num2);
+    scanf("%d", &num);
+    printf("Your new num is: %d \n", num);
+    printf("Enter a float number: \n");
+    scanf("%f", &num3);
+    printf("Your entered float number is: %.3f \n",num3);
+    printf("Now enter 2 integer, 1 float, 1 character which is new: \n");
+    scanf("%d %d %f %c",&num,&num2,&num3,&ch);
+    printf("All variables new data: %d %d %f %c",num,num2,num3,ch);
+    return 0;
+}
